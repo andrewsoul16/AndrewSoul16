9da2de8e518d350dc6566755022ebeb0c716bd73
andrewsoul16 <59179299+andrewsoul16@users.noreply.github.com>
2020-03-27 09:18:03 +0300

    Add files via upload

---
+++ DynamicArray.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class DynamicArray<A> : IEnumerable<A>
+    {
+        private A[] _array;
+        private int _size;
+        static readonly A[] _emptyArray = new A[0];
+        private const int _defaultCapacity = 8;
+
+        public DynamicArray()
+        {
+            _array = new A[8];
+            _size = 0;
+        }
+
+        public DynamicArray(int size)
+        {
+            if (size < 0)
+                throw new Exception("Размер массива не может быть отрицательным");
+            _size = 0;
+            _array = new A[size];
+        }
+
+        public DynamicArray(A[] arr)
+        {
+        
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return _array.Length;
+            }
+            set
+            {
+                if (value < _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (value != _array.Length)
+                {
+                    if (value > 0)
+                    {
+                        A[] newItems = new A[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_array, 0, newItems, 0, _size);
+                        }
+                        _array = newItems;
+                    }
+                    else
+                    {
+                        _array = _emptyArray;
+                    }
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public A this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                };
+                return _array[index];
+            }
+
+            set
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _array[index] = value;
+            }
+        }
+
+        public void Add(A item)
+        {
+            if (_size == _array.Length) 
+                EnsureCapacity(_size + 1);
+            _array[_size++] = item;
+        }
+
+        public void AddRange(A[] items)
+        {
+            if (items == null)
+                throw new ArgumentNullException();
+            if(items.Length > 0)
+            {
+                EnsureCapacity(_size + items.Length+1);
+                for(int i= 0; i < items.Length; i++)
+                {
+                    _size++;
+                    _array[_size] = items[i];
+                }
+            }
+            
+        }
+
+        public bool Remove(A item)
+        {
+            int index = Array.IndexOf(_array, item, 0, _size);
+            if (index >= 0)
+            {
+                _size--;
+                if (index < _size)
+                {
+                    Array.Copy(_array, index + 1, _array, index, _size - index);
+                }
+                _array[_size] = default(A);
+                return true;
+            }
+            return false;
+        }
+
+        public void Insert(int index, A item)
+        {
+            if ((uint)index > (uint)_size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (_size == _array.Length) EnsureCapacity(_size + 1);
+            if (index < _size)
+            {
+                Array.Copy(_array, index, _array, index + 1, _size - index);
+            }
+            _array[index] = item;
+            _size++;
+        }
+
+        private void EnsureCapacity(int min)
+        {
+            if (_array.Length < min)
+            {
+                int newCapacity = _array.Length == 0 ? _defaultCapacity : _array.Length * 2;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        /// <internalonly/>
+        IEnumerator<A> IEnumerable<A>.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public struct Enumerator : IEnumerator<A>, System.Collections.IEnumerator
+        {
+            private DynamicArray<A> dynamicArray;
+            private int index;
+            private A current;
+
+            internal Enumerator(DynamicArray<A> DynamicArray)
+            {
+                this.dynamicArray = DynamicArray;
+                index = 0;
+                current = default(A);
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+
+                DynamicArray<A> localDynamicArray = dynamicArray;
+
+                if (((uint)index < (uint)localDynamicArray._size +1))
+                {
+                    current = localDynamicArray._array[index];
+                    index++;
+                    return true;
+                }
+                return MoveNextRare();
+            }
+
+            private bool MoveNextRare()
+            {
+                index = dynamicArray._size + 1;
+                current = default(A);
+                return false;
+            }
+
+            public A Current
+            {
+                get
+                {
+                    return current;
+                }
+            }
+
+            Object System.Collections.IEnumerator.Current
+            {
+                get
+                {
+                    if (index == 0 || index == dynamicArray._size + 1)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return Current;
+                }
+            }
+
+            void System.Collections.IEnumerator.Reset()
+            {
+                index = 0;
+                current = default(A);
+            }
+
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DynamicArray<string> arr = new DynamicArray<string>();
+                bool isExit = false;
+                while (!isExit)
+                {
+                    Console.Write('>');
+                    string[] cmdArr = Console.ReadLine().Split(' ');
+                    if (cmdArr.Length > 0)
+                    {
+                        switch (cmdArr[0])
+                        {
+                            case "add":
+                                if (cmdArr.Length > 1) arr.Add(cmdArr[1]);
+                                else Console.WriteLine("Не верный синтаксис");
+                                break;
+                            case "addRange":
+                                if (cmdArr.Length > 1) arr.AddRange(cmdArr[1].Split(';'));
+                                else Console.WriteLine("Не верный синтаксис");
+                                break;
+                            case "insert":
+                                if (cmdArr.Length > 2) arr.Insert(int.Parse(cmdArr[1]), cmdArr[2]);
+                                else Console.WriteLine("Не верный синтаксис");
+                                break;
+                            case "remove":
+                                if (cmdArr.Length > 1) arr.Remove(cmdArr[1]);
+                                else Console.WriteLine("Не верный синтаксис");
+                                break;
+                            case "exit":
+                                isExit = true;
+                                break;
+                            default:
+                                Console.WriteLine("Не верная команда!");
+                                break;
+                        }
+                        foreach (var item in arr)
+                            Console.WriteLine(item);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
